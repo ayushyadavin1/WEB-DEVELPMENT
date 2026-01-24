6fe03dde7955f0d89c11fc05ca01a302650c2c78
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-24 11:43:23 +0530

    Add array creation and manipulation examples in JavaScript and HTML

--- Web.js/ArrayinJs.js
+++ Web.js/ArrayinJs.js
@@ -1,1 +1,16 @@
 let students=["AYUSH","AMAN","RAHUL"];
+
+
+//CREATING AN ARRAY
+let marks=[34,55,66,84];
+let name=["AYUSH","AMAN","RAHUL"];
+let mixedaarray=["ayush",23,45.5];
+// we can write array as a mixed array in whoch string number and floating number can store in a array 
+
+
+//IS ARRAY IS MUTABLE YES
+let fruits=["apple","banana","grapes"];
+fruits[1]="mango"; // we can change the value of array
+fruits
+//OUTPUT: ['apple', 'mango', 'grapes']
+ 
---
+++ Web.js/arraymethod.js
@@ -0,0 +1,13 @@
+// push method
+let car=["BMW","AUDI","FORD"];
+car.push("HONDA");
+car.push("ayushsystem");
+//pop method
+car.pop("ayushsystem");
+
+
+//unshift method
+car.unshift("belore","lambo");
+
+//shift method
+car.shift();
