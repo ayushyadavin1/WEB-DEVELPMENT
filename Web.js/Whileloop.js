090a067bc0e45b79b3850628a72956c01c0c8dd6
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-25 22:25:42 +0530

    Enhance Whileloop.js with movie guessing game logic and additional loop examples

--- Web.js/Whileloop.js
+++ Web.js/Whileloop.js
@@ -20,6 +20,60 @@ while ((guess!= fovmovies) && (guess!="quit")){
 }
 if(guess==fovmovies){
     console.log("you guessed it right");
-
 }
 //output
+// enter your fov mivies:abc
+// wrong guess, please try again:kfg
+// you guessed it right
+// enter your fov mivies:abc
+// wrong guess, please try again:quit
+// (no output)
+// Here in the above code we have used a while loop to allow the user to keep guessing their favorite movie until they either guess correctly or choose to quit the game.
+
+
+//for using break statement in while loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    if(i==5){
+        break;
+    }
+    i++;
+}
+//output
+// 1
+// 2
+// 3
+// 4
+// 5
+// Here in the above code we have used a while loop to print the numbers from 1 to 10 but we have used a break statement to exit the loop when the value of i is equal to 5.
+
+//loop with array
+let fruits=["apple","banana","mango","grapes"];
+for(i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+//output
+// apple
+// banana
+// mango
+// grapes
+// Here in the above code we have used a for loop to iterate through an array of fruits and print each fruit to the console.
+
+//nested loop with nested array
+let fruit=[
+    ["apple", "banana"],
+    ["mango", "grapes"]
+];
+for(i=0;i<fruit.length;i++){
+    for(j=0;j<fruit[i].length;j++){
+        console.log(fruit[i][j]);
+    }
+}
+//output
+// apple
+// banana
+// mango
+// grapes
+// Here in the above code we have used nested for loops to iterate through a nested array of fruits and print each fruit to the console.
+
