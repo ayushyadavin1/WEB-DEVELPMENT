830d0b61e5d832153b0606e617fd119bd4b37225
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-25 21:50:19 +0530

    Comment out initial while loop example and add movie guessing game logic

--- Web.js/Whileloop.js
+++ Web.js/Whileloop.js
@@ -1,9 +1,9 @@
-let i=1;
-while(i<=5)
-{
-    console.log(i);
-    i++;
-}
+// let i=1;
+// while(i<=5)
+// {
+//     console.log(i);
+//     i++;
+// }
 //output
 // 1
 // 2
@@ -11,3 +11,15 @@ while(i<=5)
 // 4
 // 5
 // Here in the above code we have used a while loop to print the numbers from 1 to 5
+
+//let us paly a game in which there will be a fov and people have to think  is the movie is correct enter or not it the assumed correct ans then display right other display pls try again but user have one choese that to quit the game
+const fovmovies="kfg";
+let guess=prompt("enter your fov mivies");
+while ((guess!= fovmovies) && (guess!="quit")){
+    guess=prompt("wrong guess, please try again");
+}
+if(guess==fovmovies){
+    console.log("you guessed it right");
+
+}
+//output
