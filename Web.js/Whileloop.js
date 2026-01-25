794b3816238ba2512fc9bb6d0fe7d528a80a4f64
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-25 18:53:58 +0530

    Remove Loop.html and Loop.js; add Whileloop.html and Whileloop.js for while loop examples

---
+++ Web.js/Whileloop.js
@@ -0,0 +1,13 @@
+let i=1;
+while(i<=5)
+{
+    console.log(i);
+    i++;
+}
+//output
+// 1
+// 2
+// 3
+// 4
+// 5
+// Here in the above code we have used a while loop to print the numbers from 1 to 5
