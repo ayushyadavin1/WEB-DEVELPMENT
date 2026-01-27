ab9e88d22871cd377305fc885e44c43e5b700230
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-27 10:55:10 +0530

    Add object literals for user details and Twitter post structure

---
+++ Web.js/objectliterals.js
@@ -0,0 +1,38 @@
+const ayushdetails={
+    name:"Ayush yadav",
+    age:21,
+    college:"VBSPU",
+    city:"jaunpur",
+    Stream:"computer science"
+
+};
+//output
+// {name: 'Ayush yadav', age: 21, college: 'VBSPU', city: 'jaunpur', Stream: 'computer science'}
+
+let delhi={
+    langitude:28.7041,
+    langitude:77.1025,
+
+};
+//output
+// {langitude: 28.7041, langitude: 77.1025}
+
+
+
+// task is creating an object literal to store the details of a twitter post in which username,content,likes,retweets,replies,tags  are included
+
+let twitter ={
+    username:"ayushyadav",
+    content:"HELLO AYUSH BHAIYA KAISE HO ",
+    likes:100,
+    retweets:50,
+    replies:20,
+    tags:["hello","world","javascript"]
+};
+//OUTPUT
+// {username: 'ayushyadav', content: 'HELLO AYUSH BHAIYA KAISE HO ', likes: 100, retweets: 50, replies: 20, tags: Array(3)}
+//array(3): ['hello', 'world', 'javascript']
+
+
+post["content"]="HELLO AYUSH BHAIYA KAISE HO , AAPKA DIN KAISE GAYA ";
+//there is the resons of doing this because of in object we use sqare braket to achieve the key valer from the whole object in a quated string.
