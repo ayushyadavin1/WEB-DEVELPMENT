a9285ba4a2be1ec2e7d4b2d93178ec5f5d7bea57
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-29 22:22:01 +0530

    Add HTML and JavaScript files for block scope and lexical scope examples

---
+++ Web.js/blockscope.js
@@ -0,0 +1,15 @@
+// {
+//  let a=25;
+// }
+// console.log(a);
+// here will be no output will be given because of variable declared inside the {}
+//and we want to print this outside the {} if we will print this inside the {}it will give resultant ans 
+// let us see how it will work
+// {
+//  let a=25;
+//  console.log(a);
+// }
+
+for (let i=1; i<=5; i++){
+    console.log(i);//block space
+}
---
+++ Web.js/lexicalscope.js
@@ -0,0 +1,8 @@
+function outerfunction(){
+    let x=5;
+    let y=6;
+function innefunc(){
+   console.log(x);
+}
+    innefunc();
+}
