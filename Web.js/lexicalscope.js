0f207350b043adc8c1b4fdcc92864de3daf7652a
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-31 23:04:49 +0530

    Add function and output examples for lexical scope demonstration

--- Web.js/lexicalscope.js
+++ Web.js/lexicalscope.js
@@ -11,3 +11,23 @@ function innerfunc(){
 //as we can see in leical scope there is two function one is inner function and another is outer function 
 //one thing is most imp that is lexical scope works from outer function to inner function 
 //also in this variable of outerfunction can be used by function variables 
+
+
+
+///PRQ 1///
+let greet="hello";//come from global scope
+function changegreet(){ //come from function scope
+    let greet="namste";
+    console.log(greet);
+    function innergreet(){
+        console.log(greet);//come from lexical scope
+    }
+
+    innergreet();
+}
+console.log(greet);
+changegreet();
+//output will be
+//hello
+//namaste
+//namaste
