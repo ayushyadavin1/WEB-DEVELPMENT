4eacc32ee87b8cf30d7e7ae8c41d7bfd7123fc67
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-01-31 22:47:23 +0530

    Add output comments and fix console log in inner function for lexical scope example

--- Web.js/lexicalscope.js
+++ Web.js/lexicalscope.js
@@ -3,6 +3,11 @@ function outerFunc(){
     let y=6;
 function innerfunc(){
    console.log(x);
+   console.log(y);
 }
     innerfunc(x);
 }
+//output
+//as we can see in leical scope there is two function one is inner function and another is outer function 
+//one thing is most imp that is lexical scope works from outer function to inner function 
+//also in this variable of outerfunction can be used by function variables 
