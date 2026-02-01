f44ee47cf2104fda90bcaca5ade430bef84fb7e9
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-02-02 00:09:08 +0530

    Add HTML and JavaScript files for arrow function and try-catch examples

---
+++ Web.js/ARROW FUNCTION.js
@@ -0,0 +1,17 @@
+const sum=(a,b)=>{
+    console.log(a+b);
+};
+
+// in arrrow function we can use this simple one line code for getting a cube of any number 
+const cube=(n)=>{
+    console.log(n*n*n);
+};
+// for getting a^b like this question 
+const pow =(a,b)=>{
+    console.log(a**b);
+
+};
+
+
+// Arrow function of implicite return 
+const mul=(a,b)=>(a*b);
---
+++ Web.js/TRY & CATCH.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+//console.log(a);if i will use this so there will an error come so if we used try and catch methology it will solve the problem 
+try{
+    console.log(a);
+
+}
+catch{
+    console.log("cought an error because of a is not defined ");
+};
+
+// catch and try always used togather whanever an code have error catch help to salve themm
