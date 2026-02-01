942fa7ef146715ad895a393f2c17f3b0f62ae15b
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-02-01 15:57:46 +0530

    Add higher order function example with multipleGreet and greet

--- Web.js/Functionexpression.js
+++ Web.js/Functionexpression.js
@@ -5,3 +5,20 @@ sum();
 //output
 //5
 //in function 
+
+
+
+// HIGHER ORDER FUNCction which take one or multiple function as arguments 
+function multipleGreet(fun,count){//higher order function 
+    for (let i=1; i<=count; i++){
+        fun();
+    }
+}
+let greet=function(){
+    console.log("hello");
+}
+
+multipleGreet(greet,1000);
+
+
+//Higher order function which return a function 
