f77e34cd8450cd1d407d4ff80628471f8be89e90
ayushyadavin1 <ayushyadavin1@gmail.com>
2026-02-02 12:03:51 +0530

    Add HTML and JavaScript files for setTimeout and setInterval examples

---
+++ Web.js/Set Timeout.js
@@ -0,0 +1,14 @@
+console.log("hi there!");
+
+setTimeout(()=>{
+    console.log("Ayush yadav");
+},4000);
+console.log("welcome to the vbspu")
+
+
+
+// set interval 
+let id =setInterval(()=>{
+    console.log("Ayush yadav");
+},2000);
+// clearInterval(id); used this when you want to stop the setinterval for repeating the value again and again 
